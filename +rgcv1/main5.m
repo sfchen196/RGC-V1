bebc2e7a3ff63c5cd1a166233a6153a5709b32a2
Shufei Chen <sfchen@brandeis.edu>
2021-01-08 14:28:25 +0800

    adds no filter versions main4.m (theirs) and main5.m (ours)

---
+++ +rgcv1/main5.m
@@ -0,0 +1,76 @@
+%% Preparation code from repo 'rwave'
+% data_mosaic = load("init_model/stat_M623_scaled.mat");
+% data_OFF = data_mosaic.mosaicOFF{1};
+% data_ON = data_mosaic.mosaicON{1};
+% 
+% data_OFF = [real(data_OFF) imag(data_OFF)];
+% data_ON = [real(data_ON) imag(data_ON)];
+% 
+% % Zero-center
+% data_center = (min([data_OFF;data_ON])+max([data_OFF;data_ON]))/2;
+% data_OFF = data_OFF-data_center;
+% data_ON = data_ON-data_center;
+
+%% Set up parameters
+crop_x = 1600;
+crop_y = 1600;
+% crop_ON = data_ON; crop_OFF = data_OFF;
+crop_ON = zeros(150,2); crop_OFF = zeros(160,2); %% toy data
+
+crop_ON(abs(crop_ON(:,1))>crop_x|abs(crop_ON(:,2))>crop_y,:) = [];
+crop_OFF(abs(crop_OFF(:,1))>crop_x|abs(crop_OFF(:,2))>crop_y,:) = [];
+
+
+pad_r = 1900;
+
+
+%% Retinal Ganglion Cell mosaics
+Result = rgcv1.RGC_mosaic(crop_ON,crop_OFF,pad_r,crop_x,crop_y);
+
+pos_ON = cell2mat(Result(1));
+pos_OFF = cell2mat(Result(2));
+d_ON = cell2mat(Result(3));
+d_OFF = cell2mat(Result(4));
+
+% figure(1);
+% ax(1) = subplot(121);plot(pos_ON(:, 1), pos_ON(:,2), '.r'); 
+% axis([-2000 2000 -2000 2000]);
+% hold on;
+% plot(pos_OFF(:, 1), pos_OFF(:,2), '.b');
+% title('ON and OFF-center RGC mosaics');
+
+%% init_V1_mosaic
+d_V1 = [];
+[pos_V1, pos_OFF] = rgcv1.init_V1_mosaic_nearest_dipole(d_V1,d_OFF,crop_x,crop_y,pos_ON,pos_OFF,2);
+figure(1);
+ax(2) = subplot(122);plot(pos_V1(:,1),pos_V1(:,2), '.r');
+linkaxes(ax,'xy');
+axis([-2000 2000 -2000 2000]);
+hold on;
+[pos_V1, pos_OFF] = rgcv1.init_V1_mosaic_nearest_dipole(d_V1,d_OFF,crop_x,crop_y,pos_ON,pos_OFF,3);
+plot(pos_V1(:,1),pos_V1(:,2), '.b');
+title('dipole sampling (red) and nearest dipole (blue)');
+
+%% statistical connectivity and receptive field computation
+imgsize_x = 200; % Filtered map width
+img_sig = 7; % Gaussian image filtering width (unit: pixels)
+ff_w0_sig = 18; % 24 for monkey/mouse, 18 for cat % Initial Exponential wiring range
+ff_w0_str = 0.05; % Initial wiring strength
+
+% increase threshold to see what changes
+ff_w0_thr = 0; % V1 selection threshold
+
+% Initial feedforward wiring
+Result = rgcv1.init_feedforward(ff_w0_sig,pos_OFF,pos_ON,pos_V1,ff_w0_str,ff_w0_thr);
+w0_V1_ON = cell2mat(Result(1));
+w0_V1_OFF = cell2mat(Result(2));
+w_V1_ON = w0_V1_ON;
+w_V1_OFF = w0_V1_OFF;
+pos_V1 = cell2mat(Result(3));
+
+Result = rgcv1.compute_OP(pos_ON,pos_OFF,w0_V1_ON,w0_V1_OFF,w_V1_ON,w_V1_OFF);
+op0 = Result(:,1);
+op0 = [op0; 0;0;0]
+op0 = reshape(op0,12,12);
+
+figure; imagesc(op0); caxis([-pi/2 pi/2]); colorbar; colormap(hsv); axis xy image
