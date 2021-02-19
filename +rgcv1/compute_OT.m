2858e24b6d20347ab6679063d623b08e7f67f3d1
Shufei Chen <sfchen@brandeis.edu>
2021-02-19 19:43:35 +0800

    adds test for compute_OT.m

--- +rgcv1/compute_OT.m
+++ +rgcv1/compute_OT.m
@@ -1,4 +1,4 @@
-function [best_response, best_response_location, selectivity] = compute_OT(CTX_RF, RF_XX, RF_YY)
+function [best_response, best_response_location, selectivity, angles] = compute_OT(CTX_RF, RF_XX, RF_YY)
 % COMPUTE_OT - compute orientation tuning for all cortical cells
 % 
 % [best_response, best_theta, selectivity] = compute_OT(CTX_RF, RF_XX, RF_YY)
@@ -34,12 +34,12 @@ CTX_RF = reshape(CTX_RF, size(CTX_RF,1), sqrt(size(CTX_RF,2)), sqrt(size(CTX_RF,
 all_responses = zeros(size(CTX_RF,1), numel(orientation));
 for i=1:n_step
     grating = repmat(gratings(:,:,i), [1,1, size(CTX_RF,1)]);
-    inputs = CTX_RF .* permute(grating,[size(CTX_RF,1), size(grating,1), size(grating,2)]);
+    inputs = CTX_RF .* permute(grating, [3 1 2]);
     response = sum(inputs,[2 3]);
     all_responses(:,i) = response;
 end
 
-[best_response,best_response_location] = max(all_responses,2);
+[best_response,best_response_location] = max(all_responses,[],2);
 orthogonal_response_location = 1 + mod(best_response_location + n_step/2 - 1, n_step);
 % code below from: https://www.mathworks.com/matlabcentral/answers/27499-how-to-select-one-element-from-each-row-of-a-matrix-a-based-on-a-column-matrix-b
 I = (1 : size(all_responses, 1)) .';
@@ -47,4 +47,6 @@ J = reshape(orthogonal_response_location, [], 1);
 k = sub2ind(size(all_responses), I, J);
 C = all_responses(k);
 
-selectivity = ( best_response - C )/ ( best_response + C );
+selectivity = ( best_response - C )./ ( best_response + C );
+
+angles = best_response_location/(n_step+1) * 2*pi;
