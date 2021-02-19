0c821a2836853868f1d3942c916a76077814eaf4
Shufei Chen <sfchen@brandeis.edu>
2021-02-19 20:39:38 +0800

    replaces for loop by matrix multiplication for better runtime

--- +rgcv1/compute_OT.m
+++ +rgcv1/compute_OT.m
@@ -21,24 +21,28 @@ function [best_response, best_response_location, selectivity, angles] = compute_
   end
   
 %   for each cell, compute the response
+gratings = reshape(gratings,[size(gratings,1)^2 size(gratings,3)]);
+all_responses = CTX_RF * gratings;
+
+% matrix multiplication equivalent to the for loop below:
+% CTX_RF = reshape(CTX_RF, size(CTX_RF,1), sqrt(size(CTX_RF,2)), sqrt(size(CTX_RF,2))); % DIM: number of cortical cells X size(RF_XX,1) X size(RF_XX,2) 
+% % ALL_RESPONSES is NxO, 
+% all_responses = zeros(size(CTX_RF,1), numel(orientation));
+% for i=1:n_step
+%     grating = repmat(gratings(:,:,i), [1,1, size(CTX_RF,1)]);
+%     inputs = CTX_RF .* permute(grating, [3 1 2]);
+%     response = sum(inputs,[2 3]);
+%     all_responses(:,i) = response;
+% end
+
+
+
 %   find the best response for each cell
 
 % BEST_RESPONSE is Nx1 (value of the best response)
 % BEST_THETA is Nx1 with the theta (O) that gives the best response
 % SELECTIVITY is Nx1 with selectivity  
 %(Response_at_best - Respose_at_best+90 degrees)/Response_at_best)
-
-
-CTX_RF = reshape(CTX_RF, size(CTX_RF,1), sqrt(size(CTX_RF,2)), sqrt(size(CTX_RF,2))); % DIM: number of cortical cells X size(RF_XX,1) X size(RF_XX,2) 
-% ALL_RESPONSES is NxO, 
-all_responses = zeros(size(CTX_RF,1), numel(orientation));
-for i=1:n_step
-    grating = repmat(gratings(:,:,i), [1,1, size(CTX_RF,1)]);
-    inputs = CTX_RF .* permute(grating, [3 1 2]);
-    response = sum(inputs,[2 3]);
-    all_responses(:,i) = response;
-end
-
 [best_response,best_response_location] = max(all_responses,[],2);
 orthogonal_response_location = 1 + mod(best_response_location + n_step/2 - 1, n_step);
 % code below from: https://www.mathworks.com/matlabcentral/answers/27499-how-to-select-one-element-from-each-row-of-a-matrix-a-based-on-a-column-matrix-b
